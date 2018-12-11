916831bcd4a2ffe28b1d2195bd7190030e59d1c3
aeduin <aeduin@nanquanu.org>
2018-12-11 17:24:49 +0100

    initial commit

--- ReversiWithAI/Form1.cs
+++ ReversiWithAI/Form1.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace ReversiWithAI {
     public partial class Form1 : Form {
         public Form1() {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
